8bb83564a25847b43d87f416cf2a752c22c1f863
Austin Burton <austin.burton426@gmail.com>
2019-09-12 15:30:44 -0600

    finished added sound and text

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import UIFx from 'uifx';
 import './App.css'
+import mp3File from './images/soundEffect.mp3';
+
+const beep = new UIFx( mp3File,{volume:1.0,throttleMs:100});
 
 class App extends React.Component{
 
@@ -8,17 +12,15 @@ state={
   result:[]
 }
 
+
+    
+    
+
 submitHandler = (e) => {
 e.preventDefault()
 //split number into an array OF numbers
-
   const arr = (this.state.number).split("");
-
-
-//the cocktail sort
-// 
-// 
-// 
+//cocktail sort below
   let is_Sorted = true;
 
   while (is_Sorted){
@@ -26,8 +28,11 @@ e.preventDefault()
              if (arr[i] > arr[i + 1])
               {
                  let temp = arr[i];
+                  // temp => a => b => temp 
                  arr[i] = arr[i + 1];
+
                  arr[i+1] = temp;
+
                  is_Sorted = true;
               }
      }
@@ -50,7 +55,8 @@ e.preventDefault()
   // console.log("Sorted array:")
   this.setState({result:arr})
   console.log(arr);
-}
+};
+
 
 //binds target value to state 
 
@@ -58,9 +64,16 @@ changeHandler = ({target}) => {
 this.setState({[target.name]: target.value});
 }
 
+
+
 render(){
   return (
     <div >
+      <div 
+      className="answer1">
+DAS COCKTAIL SORTIE BOI
+      </div>
+      
       <form 
       onSubmit={this.submitHandler}
       className="form">
@@ -74,20 +87,46 @@ render(){
       required
       />
       <button
+      className="button"
+      onClick={()=>{
+        console.log("hi")
+        return beep.play()}}
       type="submit"
       >SHAke iT lIkE a PoLaroid PicTuRe!!!</button>
       </form>
       <div 
+      
       className="answer">
-DAS COCKTAIL SORTIE BOI
+Before Some Drinks:{(this.state.number)}
       </div>
       <div 
       className="answer">
-{(this.state.number)}
+After Some Drinks:{(this.state.result).join()}
       </div>
-      <div 
-      className="answer">
-{(this.state.result).join()}
+      <div
+      className="wordyBoi"
+      >
+      <h1>Example : Let us consider an example array (5 1 4 2 8 0 2)</h1>
+
+<h2>First Forward Pass:</h2>
+<p>([5 1] 4 2 8 0 2) ? ([1 5] 4 2 8 0 2), Swap since 5 > 1</p>
+<p>(1 [5 4] 2 8 0 2) ? (1 [4 5] 2 8 0 2), Swap since 5 > 4</p>
+<p>(1 4 [5 2] 8 0 2) ? (1 4 [2 5] 8 0 2), Swap since 5 > 2</p>
+<p>(1 4 2 [5 8] 0 2) ? (1 4 2 [5 8] 0 2)</p>
+<p>(1 4 2 5 [8 0] 2) ? (1 4 2 5 [0 8] 2), Swap since 8 > 0</p>
+<p>(1 4 2 5 0 [8 2]) ? (1 4 2 5 0 [2 8]), Swap since 8 > 2</p>
+<br/>
+<h1>After first forward pass, greatest element of the array will be present at the last index of array.</h1>
+<h2>First Backward Pass:
+</h2>
+<p>(1 4 2 5 [0 2] 8) ? (1 4 2 5 [0 2] 8)</p>
+<p>(1 4 2 [5 0] 2 8) ? (1 4 2 [0 5] 2 8), Swap since 5 > 0</p>
+<p>(1 4 [2 0] 5 2 8) ? (1 4 [0 2] 5 2 8), Swap since 2 > 0</p>
+<p>(1 [4 0] 2 5 2 8) ? (1 [0 4] 2 5 2 8), Swap since 4 > 0</p>
+<p>([1 0] 4 2 5 2 8) ? ([0 1] 4 2 5 2 8), Swap since 1 > 0</p>
+<h1>After first backward pass, smallest element of the array will be present at the first index of the array.
+</h1>
+
       </div>
     </div>
   );
